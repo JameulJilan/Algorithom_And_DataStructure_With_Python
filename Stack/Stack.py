dbf944661e77771a91311ffcafbe229ddada3ca3
H. M. Jameul Haque Sarker <jameuljilan@gmail.com>
2021-12-19 00:06:45 +0600

    Update Stack.py

--- Stack/Stack.py
+++ Stack/Stack.py
@@ -1,20 +1,60 @@
+import sys
+
 class Stack:
     def __init__(self):
         self.stack=[];    
     def stack_push(self,element):
         self.stack.append(element)
     def stack_pop(self):
-        return self.stack.pop() if self.is_stack_empty()!=0 else -1 
+        return self.stack.pop()
     def stack_top(self):
-        return self.stack[0] if self.is_stack_empty()!=0 else -1 
-
+        return self.stack[len(self.stack)-1]
     def is_stack_empty(self):
-        return 1 if len(self.stack)!=0 else 0  
+        return True if len(self.stack)==0 else False  
+
+
+def numbers_to_strings(argument):
+    switcher = {
+        "1": "Push",
+        "2": "Pop",
+        "3": "Top",
+        "4": "Exit",
+    }
+
+    return switcher.get(argument, "Default")
 
 
 
 stack=Stack()
-stack.stack_push('jilan')
-print(stack.stack_pop())
-print(stack.stack_pop())
+
+while(True):
+    print('1: Push')
+    print('2: Pop')
+    print('3: Top')
+    print('4: Exit')
+    print('Enter Your Option : ')
+    choice = input()
+
+    if(numbers_to_strings(choice)=='Push'):
+         print('Enter Your Value : ')
+         value=input()
+         stack.stack_push(value)
+
+    elif(numbers_to_strings(choice)=="Pop"):
+        if(stack.is_stack_empty()):           
+            print('Stack Is Empty')
+        else:
+            print(stack.stack_pop())
+
+    elif(numbers_to_strings(choice)=="Top"):
+        if(stack.is_stack_empty()):
+            print('Stack Is Empty')
+        else:
+            print(stack.stack_top())
+
+    elif(numbers_to_strings(choice)=="Exit"):
+        sys.exit()
+    else:
+        print('Envalid Input.')
+        
 
